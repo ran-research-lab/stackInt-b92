bda1578bfd1cb59cb86c5d18fcf36c3884329a9c
Rafael Arce Nazario <rafael.arce@upr.edu>
2020-03-27 10:29:26 -0400

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Stack implementado usando arreglo dinámico
+
+Implemente las member functions (dentro de stackInt.cpp)
+
+Observe:
+* estoy usando `_top` para el data member y `top` para la función
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#define CATCH_CONFIG_MAIN
+
+
+#include "catch.hpp"
+#include "stackInt.h"
+#include <iostream>
+
+using namespace std;
+
+
+
+// Test cases for the stackInt class
+
+TEST_CASE( "stackInt", "[stackInt]" ) {
+    stackInt S;
+    CHECK(S.empty() == true);
+    // cree sus propias pruebas que validen el resto de las funciones
+    // (excepto la del destructor)
+}
